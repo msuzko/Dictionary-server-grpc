4cb84436bb183c2ab8ef35adf280616687d980f3
msuzko <msuzko@gmail.com>
2023-06-06 13:03:31 +0300

    first commit

---
+++ src/main/java/com/mec/dictionarygrpcserver/Application.java
@@ -0,0 +1,13 @@
+package com.mec.dictionarygrpcserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
